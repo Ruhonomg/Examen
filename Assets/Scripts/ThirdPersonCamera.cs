6aaa88ab26935038e4dca363e65ea623d7fbbcc0
Ruhonomg <Rubencamaron10@gmail.com>
2021-10-27 09:29:17 +0200

    ExamV3

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Vector3 offset;
+    [Range(0, 1)]public float lerpValue;
+    public float sensibilidad;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, lerpValue);
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensibilidad, Vector3.up) * offset;
+
+        transform.LookAt(target);
+    }
+}
