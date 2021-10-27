259576dc89228cb66f535ef17d3fdad1c14bd2a8
Ruhonomg <Rubencamaron10@gmail.com>
2021-10-27 10:14:48 +0200

    ExamV4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     private float horizontalMove;
     private float verticalMove;
+    private Vector3 playerInput;
+    private Vector3 camForward;
+    private Vector3 camRight;
+    private Vector3 movePlayer;
 
     public CharacterController player;
     public float playerSpeed;
+    public Camera mainCamera;
+    public float gravity = 9.8f;
+    public float fallVelocity;
+    public float jumpForce;
 
     void Start()
     {
@@ -19,10 +27,59 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        movePlayer = movePlayer * playerSpeed;
+
+        SetGravity();
+
+        PlayerSkills();
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        player.Move(movePlayer * Time.deltaTime);
+
     }
 
-    private void FixedUpdate()
+    public void camDirection()
     {
-        player.Move(new Vector3(horizontalMove, 0, verticalMove) * playerSpeed * Time.deltaTime);
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    public void SetGravity()
+    {
+        if(player.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+    }
+
+    public void PlayerSkills()
+    {
+        if(player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
+        }
     }   
+   
 }
